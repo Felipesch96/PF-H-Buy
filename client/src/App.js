54212a0e126c3bf4a54441a76d077eb2a95e9086
EmijHernandez <100807932+emijhernandez@users.noreply.github.com>
2023-02-14 20:29:26 -0300

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Home from "./components/views/home"
+
+import{
+  BrowserRouter as Router,
+  Route
+}from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' component={Home}/>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/views/home/Home.jsx
+++ client/src/components/views/home/Home.jsx
@@ -3,6 +3,8 @@ import React from "react";
 export default function Home(){
 
   return (
-    <div></div>
+    <div>
+      <h1>trans</h1>
+    </div>
   )
 }
