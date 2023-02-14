93bc337619a92244e48c555ecb07e4f8c029988b
Felipe Schwindt <felipe.schwindt36@gmail.com>
2023-02-14 12:37:47 -0300

    Product-Schema

--- api/src/schemas/Products.js
+++ api/src/schemas/Products.js
@@ -1,13 +1,50 @@
 const mongoose = require("mongoose");
 
-const products = mongoose.Schema(
-  {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+const products = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
   },
-  { timestamps: true }
-);
+  img: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  score: {
+    type: Number,
+  },
+  review: {
+    type: String,
+  },
+  stock: {
+    type: Number,
+    require: true,
+  },
+  seller_id: {
+    type: Number,
+    require: true,
+  },
+  category: {
+    type: mongoose.Types.ObjectId,
+    ref: "Categories",
+    require: true,
+  },
+  created: {
+    type: Date,
+    inmutable: true,
+    default: () => Date.now(),
+  },
+  modified: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
 
 module.exports = mongoose.model("Products", products);
