b4fe63c3f4ae1db6b8d6036dbbbfab6662d922ef
Cesar Leyton <cesarleyton549@gmail.com>
2023-02-14 22:11:59 -0500

    Agregados los slices a a la store

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authSlice } from './slices/authSlice'
+import { productsSlice } from './slices/productsSlice'
+import { cartSlice } from './slices/cartSlice'
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer
+    auth: authSlice.reducer,
+    product: productsSlice.reducer,
+    cart: cartSlice.reducer
   },
 })
