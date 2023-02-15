8967337a68cee79d5b4238fa8cc427a690552d21
EmijHernandez <100807932+emijhernandez@users.noreply.github.com>
2023-02-15 04:49:44 -0300

    cambios landing

--- client/src/components/views/about/About.jsx
+++ client/src/components/views/about/About.jsx
@@ -5,52 +5,52 @@ import github from "../landingPage/github.png"
 const About=()=>{
     return(
         <div className="bg-success-subtle">
-        <h1 className="text-center">Sobre Nosotros</h1>
+        <h1 className="text-center">ABOUT US</h1>
         <div className="row row-cols-2 row-cols-lg-4 g-2 g-lg-3 rounded float-start" style={{textAlign:"center"}}>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga" className="border border-dark border-2 rounded-circle"  />
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
           </div>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga"  className="border border-dark border-2 rounded-circle"/>
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
           </div>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga"  className="border border-dark border-2 rounded-circle"/>
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
           </div>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga"  className="border border-dark border-2 rounded-circle"/>
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
           </div>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga"  className="border border-dark border-2 rounded-circle"/>
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
           </div>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga"  className="border border-dark border-2 rounded-circle"/>
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
           </div>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga"  className="border border-dark border-2 rounded-circle"/>
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
           </div>
           <div>
-            <img src={random} alt="no se carga" style={{borderRadius:"100px"}} />
+            <img src={random} alt="no se carga" className="border border-dark border-2 rounded-circle"/>
             <h5>Emiliano Hernandez</h5>
             <a href="https://github.com/emijhernandez"><img src={linkedin} alt="" style={{width:"50px"}} /> </a>
             <a href="https://github.com/emijhernandez"><img src={github} alt="" style={{width:"50px"}}/> </a>
--- client/src/components/views/landingPage/landingPage.jsx
+++ client/src/components/views/landingPage/landingPage.jsx
@@ -1,18 +1,31 @@
 import styles from "./landingPage.module.css";
+import Carousel from "../home/Carousel"
 
 const LandingPage = () => {
   return (
     <div className={styles.landing}>
       <div>
-        <div className="row row-cols-3 g-1 bg-dark bg-opacity-50">
-          <br />
-          <br />
-          <br />
-        <a href="#about" className="text-center"> 
-          <button>Sobre Nosotros</button>
-        </a>
-        <h1 className="text-center">Bienvenidos a H-Buy</h1>
+        <div className="bg-dark bg-opacity-50">
+          <h1 className="text-center">Welcome to H-Buy</h1>
         </div>
+        <div className="card" style={{width:"250px"}}>
+          <img
+            src="https://resource.logitech.com/w_800,c_lpad,ar_1:1,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/mice/m190-wireless-mouse/m190-wireless-mouse-charcoal-gallery-01.png?v=1"
+            className="card-img-top"
+            alt="Placeholder"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <Carousel/>
         <p>
           Lorem ipsum dolor sit
           <br /> amet consectetur adipisicing elit. Aut,
@@ -27,8 +40,8 @@ const LandingPage = () => {
           Lorem ipsum dolor sit
           <br /> amet consectetur adipisicing elit. Aut,
           <br /> excepturi esse nemo
-          <br /> similique nobis incidunt molestias hic beatae assumenda
-          sequi inventore reprehen
+          <br /> similique nobis incidunt molestias hic beatae assumenda sequi
+          inventore reprehen
           <br />
           derit laudantium eveniet fugiat repellat
           <br />, natus culpa impedit in!
@@ -37,8 +50,8 @@ const LandingPage = () => {
           Lorem ipsum dolor sit
           <br /> amet consectetur adipisicing elit. Aut,
           <br /> excepturi esse nemo
-          <br /> similique nobis incidunt molestias hic beatae assumenda
-          sequi inventore reprehen
+          <br /> similique nobis incidunt molestias hic beatae assumenda sequi
+          inventore reprehen
           <br />
           derit laudantium eveniet fugiat repellat
           <br />, natus culpa impedit in!
@@ -52,8 +65,11 @@ const LandingPage = () => {
           derit laudantium eveniet fugiat repellat
           <br />, natus culpa impedit in!
         </p>
-        <a href="/home"><button type="button" class="btn btn-secondary text-center col-6 p-3">
-          Ir al Home.</button></a>
+        <a href="/home">
+          <button type="button" class="btn btn-secondary text-center col-6 p-3">
+            Ir al Home.
+          </button>
+        </a>
 
         <br />
         <br />
