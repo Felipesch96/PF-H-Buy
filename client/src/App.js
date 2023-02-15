a4494c12b06b952bdaf005e29f5b1b262c8bd29c
Nicolas <nicolas.iconicoff@gmail.com>
2023-02-14 21:18:49 -0300

    routas componente App

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from "./components/Home";
+import Home from "./components/views/home/Home";
 import NavBar from "./components/navBar/NavBar";
+import ProductsPage from './components/views/productsPage/ProductsPage';
+import Footer from './components/views/footer/Footer';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
         <Switch>
           {/* <Route exact path='/' component={LandingPage} /> */}
           <Route exact path='/home' component={Home} />
+          <Route exact path='/products' component={ProductsPage} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
