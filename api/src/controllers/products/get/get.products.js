64ddcfb66a84748fa325190ec0ff58bf74549632
laraonys <lara.onys@gmail.com>
2023-02-16 17:24:20 -0300

    filtersRedone

--- api/src/controllers/products/get/get.products.js
+++ api/src/controllers/products/get/get.products.js
@@ -6,33 +6,34 @@ productsCtrl.getProducts = async (req, res) => {
   const { name, category, priceMin, priceMax, brand, condition } = req.query;
   try {
     if (name || category || priceMin || priceMax || brand || condition) {
-      // if (category !== undefined && typeof category !== "string")
-      //   return res.status(400).send({ error: `Category should be a string` });
-      // if (name !== undefined && typeof name !== "string")
-      //   return res.status(400).send({ error: `Name should be a string` });
-      // if (
-      //   priceMin !== undefined &&
-      //   priceMax !== undefined &&
-      //   typeof priceMin !== "number" &&
-      //   typeof priceMax !== "number"
-      // )
-      //   return res.status(400).send({ error: `Price should be a number` });
-      // if (brand !== undefined && typeof brand !== "string")
-      //   return res.status(400).send({ error: `Brand should be a string` });
-      // if (condition !== undefined && typeof condition !== "string")
-      //   return res.status(400).send({ error: `Condition should be a string` });
-
-      const allProducts = await Product.find({
-        // category: category,
-        name: name && new RegExp(name, "i"),
-        // brand: brand,
-        // condition: condition,
-      });
-      allProducts.length
-        ? res.status(200).send(allProducts)
-        : res
-            .status(202)
-            .send({ error: `There are no products in the DataBase` });
+      if (category !== undefined && typeof category === "string") {
+        const allProducts = await Product.find({
+          category: category
+        });
+        return res.status(200).send(allProducts)
+      } else if (name !== undefined && typeof name === "string") {
+        const allProducts = await Product.find({
+          name: name && new RegExp(name, "i")
+        });
+        return res.status(200).send(allProducts)
+      } else if (priceMin !== undefined && priceMax !== undefined && typeof priceMin === "number" && typeof priceMax === "number") {
+        const allProducts = await Product.find({
+          price: hola
+        })
+        return res.status(200).send(allProducts)
+      } else if (brand !== undefined && typeof brand === "string") {
+        const allProducts = await Product.find({
+          brand: brand
+        });
+        return res.status(200).send(allProducts)
+      } else if (condition !== undefined && typeof condition === "string") {
+        const allProducts = await Product.find({
+          condition: condition
+        });
+        return res.status(200).send(allProducts)
+      } else {
+        res.status(202).send({ error: `There are no products in the DataBase` });
+      }
     } else {
       const allProducts = await Product.find();
       allProducts.length
