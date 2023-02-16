663b551d2a3c1b7b30e13b77ec5fa333972440dd
Felipe Schwindt <felipe.schwindt36@gmail.com>
2023-02-16 17:02:02 -0300

    new way to filter

--- api/src/controllers/products/filters/filter2.js
+++
@@ -1,8 +0,0 @@
-const Filter = require("../../../schemas/Filter");
-
-const filter2 = async (req, res) => {
-  const allFilter = await Filter.find();
-  res.status(200).send(allFilter);
-};
-
-module.exports = filter2;
--- api/src/controllers/products/get/get.products.js
+++ api/src/controllers/products/get/get.products.js
@@ -3,10 +3,31 @@ const Product = require("../../../schemas/Products");
 const productsCtrl = {};
 
 productsCtrl.getProducts = async (req, res) => {
-  const name = req.query.name;
+  const { name, category, priceMin, priceMax, brand, condition } = req.query;
   try {
-    if (!name) {
-      const allProducts = await Product.find();
+    if (name || category || priceMin || priceMax || brand || condition) {
+      // if (category !== undefined && typeof category !== "string")
+      //   return res.status(400).send({ error: `Category should be a string` });
+      // if (name !== undefined && typeof name !== "string")
+      //   return res.status(400).send({ error: `Name should be a string` });
+      // if (
+      //   priceMin !== undefined &&
+      //   priceMax !== undefined &&
+      //   typeof priceMin !== "number" &&
+      //   typeof priceMax !== "number"
+      // )
+      //   return res.status(400).send({ error: `Price should be a number` });
+      // if (brand !== undefined && typeof brand !== "string")
+      //   return res.status(400).send({ error: `Brand should be a string` });
+      // if (condition !== undefined && typeof condition !== "string")
+      //   return res.status(400).send({ error: `Condition should be a string` });
+
+      const allProducts = await Product.find({
+        // category: category,
+        name: name && new RegExp(name, "i"),
+        // brand: brand,
+        // condition: condition,
+      });
       allProducts.length
         ? res.status(200).send(allProducts)
         : res
@@ -14,18 +35,11 @@ productsCtrl.getProducts = async (req, res) => {
             .send({ error: `There are no products in the DataBase` });
     } else {
       const allProducts = await Product.find();
-      const productByName = allProducts.filter((element) =>
-        element.name
-          .toString()
-          .trim()
-          .toLowerCase()
-          .includes(name.toLowerCase())
-      );
-      productByName.length
-        ? res.status(200).send(productByName)
+      allProducts.length
+        ? res.status(200).send(allProducts)
         : res
             .status(202)
-            .send({ error: `No products found with the name ${name}` });
+            .send({ error: `There are no products in the DataBase` });
     }
   } catch (error) {
     res.status(400).send({ error: error.message });
