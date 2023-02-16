a4c659a845ea9404ccf5821d7bf63890ffc97b9b
Felipe Schwindt <felipe.schwindt36@gmail.com>
2023-02-16 15:45:18 -0300

    fixed main switch for filters

--- api/src/controllers/products/filters/helpers/category.filter.js
+++ api/src/controllers/products/filters/helpers/category.filter.js
@@ -3,12 +3,10 @@ const Filter = require("../../../../schemas/Filter");
 
 const filterByCategory = async (name, aux) => {
   if (!aux) {
-    console.log("primero");
     const filtered = await Product.find({ category: name });
     return filtered;
   } else {
     const filtered = aux.filter((element) => element.category === name);
-    console.log("segundo");
     await Filter.deleteMany();
     return filtered;
   }
--- api/src/controllers/products/filters/helpers/main.js
+++ api/src/controllers/products/filters/helpers/main.js
@@ -8,63 +8,38 @@ const filter = async (type, name) => {
   const filters = await Filter.find();
   filters.length ? (aux = filters) : (aux = null);
 
+  function auxFunction(filter) {
+    filter.forEach(async (element) => {
+      const newFilter = new Filter({
+        name: element.name,
+        category: element.category,
+        brand: element.brand,
+        condition: element.condition,
+        price: element.price,
+        payment: element.payment,
+      });
+      await newFilter.save();
+    });
+    return filter;
+  }
+
   switch (type) {
     case "category":
       const filterCat = await filterByCategory(name, aux);
-      filterCat.forEach(async (element) => {
-        const newFilter = new Filter({
-          name: element.name,
-          category: element.category,
-          brand: element.brand,
-          condition: element.condition,
-          price: element.price,
-          payment: element.payment,
-        });
-        await newFilter.save();
-      });
-      return filterCat;
+      const auxCat = await auxFunction(filterCat);
+      return auxCat;
     case "price":
       const filterPrice = await filterByPrice(name, aux);
-      filterPrice.forEach(async (element) => {
-        const newFilter = new Filter({
-          name: element.name,
-          category: element.category,
-          brand: element.brand,
-          condition: element.condition,
-          price: element.price,
-          payment: element.payment,
-        });
-        await newFilter.save();
-      });
-      return filterPrice;
+      const auxPrice = await auxFunction(filterPrice);
+      return auxPrice;
     case "brand":
       const filterBrand = await filterByBrand(name, aux);
-      filterBrand.forEach(async (element) => {
-        const newFilter = new Filter({
-          name: element.name,
-          category: element.category,
-          brand: element.brand,
-          condition: element.condition,
-          price: element.price,
-          payment: element.payment,
-        });
-        await newFilter.save();
-      });
-      return filterBrand;
+      const auxBrand = await auxFunction(filterBrand);
+      return auxBrand;
     case "newOrUsed":
       const filterCond = await filterByNewOrUsed(name, aux);
-      filterCond.forEach(async (element) => {
-        const newFilter = new Filter({
-          name: element.name,
-          category: element.category,
-          brand: element.brand,
-          condition: element.condition,
-          price: element.price,
-          payment: element.payment,
-        });
-        await newFilter.save();
-      });
-      return filterCond;
+      const auxCond = await auxFunction(filterCond);
+      return auxCond;
     default:
       return filter;
   }
