4b0a6d492a64b4bb2f229cf8410e0d27d2fa2a07
cristopher-99 <cristo_lazo_@hotmail.com>
2023-02-15 22:53:13 -0300

    render cards and paginate in home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from "./components/Home";
+import Home from "./components/views/home/Home";
 import NavBar from "./components/navBar/NavBar";
+import ProductsPage from './components/views/productsPage/ProductsPage';
+import Footer from './components/views/footer/Footer';
+import LandingPage from './components/views/landingPage/landingPage';
+import About from './components/views/about/About';
 
 function App() {
   return (
@@ -8,9 +12,12 @@ function App() {
       <Router>
         <NavBar />
         <Switch>
-          {/* <Route exact path='/' component={LandingPage} /> */}
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/about' component={About} />
           <Route exact path='/home' component={Home} />
+          <Route exact path='/products' component={ProductsPage} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
---
+++ client/src/components/views/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousel from "./Carousel";
+import Cards from "../../Cards/Cards";
+const Home = () => {
+  return (
+    <div>
+      <Carousel />
+      <Cards />
+    </div>
+  );
+};
+
+export default Home;
