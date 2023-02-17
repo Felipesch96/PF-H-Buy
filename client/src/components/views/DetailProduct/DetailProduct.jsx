d94621771a0bd9dc23ae91d22d1e2241f1e01e7c
cristopher-99 <cristo_lazo_@hotmail.com>
2023-02-17 14:20:01 -0300

    modificacion para component-detail

---
+++ client/src/components/views/DetailProduct/DetailProduct.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DetailProduct = (props) => {
+  return (
+    <div className="container">
+      <h1> Hola soy el detalle del producto ;C</h1>
+    </div>
+  );
+};
+
+export default DetailProduct;
