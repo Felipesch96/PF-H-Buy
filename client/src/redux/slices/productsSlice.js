982f2e550a2b2cbd95680bc2254c8b7140ef9336
cristopher-99 <cristo_lazo_@hotmail.com>
2023-02-17 21:06:25 -0300

    update detail-product

--- client/src/redux/slices/productsSlice.js
+++ client/src/redux/slices/productsSlice.js
@@ -1,33 +1,59 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const productsSlice = createSlice({
-    name: "productSlice",
-    initialState: {
-      products: [],
-      categories : []
+  name: "productSlice",
+  initialState: {
+    products: [],
+    categories: [],
+    detailproduct: {},
+  },
+  reducers: {
+    setProducts: (state, { payload }) => {
+      state.products = payload;
     },
-    reducers: {
-      setProducts: (state, {payload}) => {
-        state.products = payload;
-      },
-      setCategories : (state,{payload}) => {
-        state.categories = payload
-      },
-      orderByName: (state, {payload}) => {
-        payload === 'A-Z' ? state.products = [...state.products].sort((a,b) => a.name-b.name) :
-        state.products = [...state.products].sort((a,b) => b.name-a.name)
-      },
-      orderByPrice: (state, {payload}) => {
-        payload === 'incremental' ? state.products = [...state.products].sort((a,b) => a.price-b.price) :
-        state.products = [...state.products].sort((a,b) => b.price-a.price)
-      },
-      orderByReviews: (state, {payload}) => {
-        payload === 'incremental' ? state.products = [...state.products].sort((a,b) => a.reviews-b.reviews) :
-        state.products = [...state.products].sort((a,b) => b.reviews-a.reviews)
-      }
+    setCategories: (state, { payload }) => {
+      state.categories = payload;
+    },
+    orderByName: (state, { payload }) => {
+      payload === "A-Z"
+        ? (state.products = [...state.products].sort((a, b) => a.name - b.name))
+        : (state.products = [...state.products].sort(
+            (a, b) => b.name - a.name
+          ));
+    },
+    orderByPrice: (state, { payload }) => {
+      payload === "incremental"
+        ? (state.products = [...state.products].sort(
+            (a, b) => a.price - b.price
+          ))
+        : (state.products = [...state.products].sort(
+            (a, b) => b.price - a.price
+          ));
+    },
+    orderByReviews: (state, { payload }) => {
+      payload === "incremental"
+        ? (state.products = [...state.products].sort(
+            (a, b) => a.reviews - b.reviews
+          ))
+        : (state.products = [...state.products].sort(
+            (a, b) => b.reviews - a.reviews
+          ));
+    },
 
-    }
-  });
+    detailProduct: (state, { payload }) => {
+      state.detailproduct = payload;
+    },
+    // clearDetail: (state) => {
+    //   state.detailproduct = {};
+    // },
+  },
+});
 
-
-export const {setProducts , setCategories, orderByName , orderByPrice , orderByReviews} = productsSlice.actions
+export const {
+  setProducts,
+  setCategories,
+  orderByName,
+  orderByPrice,
+  orderByReviews,
+  detailProduct,
+} = productsSlice.actions;
