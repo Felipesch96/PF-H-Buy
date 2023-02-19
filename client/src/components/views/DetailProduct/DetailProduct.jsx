3e8c0abdae5fc9dfe2c869fb6581c3c0bedf54bd
cristopher-99 <cristo_lazo_@hotmail.com>
2023-02-19 17:34:11 -0300

    update detail-tabs

--- client/src/components/views/DetailProduct/DetailProduct.jsx
+++ client/src/components/views/DetailProduct/DetailProduct.jsx
@@ -28,7 +28,6 @@ const DetailProduct = () => {
               <div class="col-7 col-sm-8">
                 <div class="card-body">
                   <h2 class="card-title">{detailProduct.name}</h2>
-                  <p class="card-text">{detailProduct.description}</p>
                   <p class="card-text">Stock : {detailProduct.stock}</p>
                   <p
                     class="card-text bg-success text-white rounded-2"
@@ -54,6 +53,72 @@ const DetailProduct = () => {
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </p>
                 </div>
+              </div>
+            </div>
+            <nav>
+              <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <button
+                  class="nav-link active"
+                  id="nav-home-tab"
+                  data-bs-toggle="tab"
+                  data-bs-target="#nav-home"
+                  type="button"
+                  role="tab"
+                  aria-controls="nav-home"
+                  aria-selected="true"
+                >
+                  Description
+                </button>
+                <button
+                  class="nav-link"
+                  id="nav-profile-tab"
+                  data-bs-toggle="tab"
+                  data-bs-target="#nav-profile"
+                  type="button"
+                  role="tab"
+                  aria-controls="nav-profile"
+                  aria-selected="false"
+                >
+                  Caracteristicas
+                </button>
+                <button
+                  class="nav-link"
+                  id="nav-contact-tab"
+                  data-bs-toggle="tab"
+                  data-bs-target="#nav-contact"
+                  type="button"
+                  role="tab"
+                  aria-controls="nav-contact"
+                  aria-selected="false"
+                >
+                  Reviews
+                </button>
+              </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+              <div
+                class="tab-pane fade show active"
+                id="nav-home"
+                role="tabpanel"
+                aria-labelledby="nav-home-tab"
+              >
+                <p class="card-text m-3">{detailProduct.description}</p>
+              </div>
+              <div
+                class="tab-pane fade"
+                id="nav-profile"
+                role="tabpanel"
+                aria-labelledby="nav-profile-tab"
+              >
+                ...
+              </div>
+              <div
+                class="tab-pane fade"
+                id="nav-contact"
+                role="tabpanel"
+                aria-labelledby="nav-contact-tab"
+              >
+                ...
               </div>
             </div>
           </div>
