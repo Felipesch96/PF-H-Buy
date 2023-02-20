074d2db218f87e1ea93c84cf90e9d81fdf48a176
Cesar Leyton <cesarleyton549@gmail.com>
2023-02-20 17:32:08 -0500

    Añadido el register

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/views/home/Home";
 import NavBar from "./components/navBar/NavBar";
 import ProductsPage from './components/views/productsPage/ProductsPage';
 import Footer from './components/views/footer/Footer';
-import LandingPage from './components/views/LandingPage/LandingPage'
+import LandingPage from './components/views/LandingPage'
 import About from './components/views/about/About';
 import ProfileComponent from './components/views/profiles/profileComponent/ProfileComponent';
 import DetailProduct from "./components/views/DetailProduct/DetailProduct";
@@ -23,7 +23,6 @@ const App =()=>{
           <Route exact path='/profile' component={ProfileComponent} />
           <Route exact path="/products/:id" component={DetailProduct} />
           <Route exact path="/signup" component={SignUp} />
-
         </Switch>
         <Footer />
       </Router>
