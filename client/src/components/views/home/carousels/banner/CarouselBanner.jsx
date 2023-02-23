77b42cd75814490ce7a5757ca1a178480bbda712
Nicolas <nicolas.iconicoff@gmail.com>
2023-02-22 22:23:54 -0300

    cambios carousel home

--- client/src/components/views/home/carousels/banner/CarouselBanner.jsx
+++ client/src/components/views/home/carousels/banner/CarouselBanner.jsx
@@ -21,18 +21,25 @@ const CarouselBanner = () => {
     <div id="carouselExampleAutoplaying" class="carousel slide contenedor" data-bs-ride="carousel"
     >
       <div class="carousel-inner tamano">
-        {
+        {/* {
           images?.map(i => {
             return (
               <div class="carousel-item active contenedor-imagen">
                 <img src={require(`../media/${i}`)} class="img-fluid imagen" alt="img-fluid"
-                  // style={{ width: "200vw" }}
                 ></img>
-                {/* <img src={require(`./media/${i}`)} class="imagen" alt="..." /> */}
               </div>
             )
           })
-        }
+        } */}
+        <div class="carousel-item active">
+          <img src={require(`../media/baner0.jpg`)} class="d-block w-100 contenedor-imagen" alt="..."/>
+        </div>
+        <div class="carousel-item">
+          <img src={require(`../media/baner1.jpg`)} class="d-block w-100 contenedor-imagen" alt="..."/>
+        </div>
+        <div class="carousel-item">
+          <img src={require(`../media/baner2.jpg`)} class="d-block w-100 contenedor-imagen" alt="..."/>
+        </div>
         <button class="carousel-control-prev " type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
           <span class="carousel-control-prev-icon"></span>
           <span className="prev-next" ><b>Previous</b></span>
