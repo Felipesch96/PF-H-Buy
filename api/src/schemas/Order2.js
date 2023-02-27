0233b7dfbed51fe6a085dab33485ba307c4cec47
Your Name <you@example.com>
2023-02-27 10:03:33 -0300

    comit

---
+++ api/src/schemas/Order2.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  buyer_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "Users",
+  },
+  items: [
+    {
+      product: {
+        type: mongoose.Types.ObjectId,
+        ref: "Products",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  // shippingAddress: {
+  //   fullName: { type: String, required: true },
+  //   address: { type: String, required: true },
+  //   city: { type: String, required: true },
+  //   postalCode: { type: String, required: true },
+  //   country: { type: String, required: true },
+  // },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  delivered: {
+    type: Boolean,
+    default: false,
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+  // payment: {
+
+  // }
+});
+
+module.exports = mongoose.model("Order", orderSchema);
