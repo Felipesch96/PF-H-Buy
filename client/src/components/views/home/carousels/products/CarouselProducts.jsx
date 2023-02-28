5c5d892367f00692cb3ea6c7a2a6b161175fc693
Felipe Schwindt <felipe.schwindt36@gmail.com>
2023-02-28 18:17:43 -0300

    carousel update

--- client/src/components/views/home/carousels/products/CarouselProducts.jsx
+++ client/src/components/views/home/carousels/products/CarouselProducts.jsx
@@ -1,54 +1,135 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux"
 import { fetchProducts } from "../../../../../redux/thunks/productThunk";
 import CarouselCard from "./CarouselCard";
 import "./CarouselProducts.css";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
+// const CarouselProducts = () => {
+//   // se le podria pasar los productos por params 
+// const dispatch = useDispatch();
+
+// const { products } = useSelector((state) => state.product);
+
+//   const [pageCurrent, setPageCurrent] = useState(1);
+//   const [cardsPerPage, setCardsPerPage] = useState(6);
+//   const indexLastCard = pageCurrent * cardsPerPage;
+//   const indexFirstCard = indexLastCard - cardsPerPage;
+// const cardsCurrent = products?.slice(indexFirstCard, indexLastCard);
+
+//   // const paginado = (page) => {
+//   //   setPageCurrent(page);
+//   // };
+//   const selectNewImage = (index, images, next = true) => {
+//     setTimeout(() => {
+//       const condition = next
+//         ? pageCurrent < 2
+//         : pageCurrent > 1;
+//       const nextIndex = next
+//         ? condition
+//           ? pageCurrent + 1
+//           : 1
+//         : condition
+//           ? pageCurrent - 1
+//           : 2;
+//       setPageCurrent(products[nextIndex]);
+//       setPageCurrent(nextIndex);
+//     }, 500);
+//   };
+
+//   const previous = () => {
+//     selectNewImage(pageCurrent, products, false);
+//   };
+//   const next = () => {
+//     selectNewImage(pageCurrent, products);
+//   };
+
+//   useEffect(() => {
+//     const reloj = setInterval(() => {
+//       selectNewImage(pageCurrent, products);
+//     }, 2500);
+//     return () => clearInterval(reloj);
+//   });
+
+//   useEffect(() => {
+//     dispatch(fetchProducts());
+//     // dispatch(fetchCategories());
+//   }, [dispatch]);
+
+//   return (
+//     <div className="d-grid gap-2 d-flex contenedor-products">
+
+//       <button class="btn btn-primary box" type="button" onClick={previous}>{"<"}</button>
+//       <div class="d-flex justify-content-center align-items-center">
+//         {cardsCurrent.map((p) => {
+//           return (
+//             <div class="box">
+//               <CarouselCard
+//                 _id={p._id}
+//                 img={p.img}
+//                 name={p.name}
+//                 score={p.score}
+//               />
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <button class="btn btn-primary box" type="button" onClick={next}>{">"}</button>
+//     </div>
+//   )
+// };
+
+// export default CarouselProducts;
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const CarouselProducts = () => {
   const dispatch = useDispatch();
 
   const { products } = useSelector((state) => state.product);
+  const total = products?.slice(0, 10);
 
-  const [pageCurrent, setPageCurrent] = useState(1);
-  const [cardsPerPage] = useState(6);
-  const indexLastCard = pageCurrent * cardsPerPage;
-  const indexFirstCard = indexLastCard - cardsPerPage;
-  const cardsCurrent = products?.slice(indexFirstCard, indexLastCard);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true, // Reproduce el carrusel automáticamente
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
 
   // const paginado = (page) => {
   //   setPageCurrent(page);
   // };
-  const selectNewImage = (next = true) => {
-    setTimeout(() => {
-      const condition = next ? pageCurrent < 2 : pageCurrent > 1;
-      const nextIndex = next
-        ? condition
-          ? pageCurrent + 1
-          : 1
-        : condition
-          ? pageCurrent - 1
-          : 2;
-      setPageCurrent(products[nextIndex]);
-      setPageCurrent(nextIndex);
-    }, 500);
-  };
-
-  const previous = () => {
-    selectNewImage(pageCurrent, products, false);
-  };
-  const next = () => {
-    selectNewImage(pageCurrent, products);
-  };
-
-  useEffect(() => {
-    const reloj = setInterval(() => {
-      selectNewImage(pageCurrent, products);
-    }, 2500);
-    return () => clearInterval(reloj);
-  });
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -56,34 +137,23 @@ const CarouselProducts = () => {
   }, [dispatch]);
 
   return (
-    <div className="d-grid gap-3 d-flex contenedor-products">
-      {/* <Slider {...settings}>
-      {total.map((p) => { */}
-      <button class="btn btn-primary box" type="button" onClick={previous}>
-        {"<"}
-      </button>
-      {/* <div class="container"> */}
-      <div className="fila-products">
-        {/* <div class="row"> */}
-
-        {cardsCurrent.map((span) => {
+    <div>
+      <Slider {...settings}>
+      {total.map((p) => {
           return (
-            <div key={span._id} class="col">
+            <div key={p._id} class=" box">
               <CarouselCard
-                _id={span._id}
-                img={span.img}
-                name={span.name}
-                score={span.score}
+                _id={p._id}
+                img={p.img}
+                name={p.name}
+                score={p.score}
               />
             </div>
           );
         })}
-      </div>
-      {/* </div> */}
-      <button class="btn btn-primary box" type="button" onClick={next}>
-        {">"}
-      </button>
-      {/* </Slider> */}
+      </Slider>
+      <br />
+
     </div>
   );
 };
