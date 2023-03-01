c55ebcffccd3b0086e55363023be22200ac5f34b
laraonys <lara.onys@gmail.com>
2023-03-01 17:44:48 -0300

    alertcart & filterSearch fixed

--- client/src/components/CartCard/CartCard.jsx
+++ client/src/components/CartCard/CartCard.jsx
@@ -1,13 +1,20 @@
 import { useDispatch } from "react-redux";
-import {
-  decrementQuantity,
-  incrementQuantity,
-  removeFromCart,
-} from "../../redux/slices/cartSlice";
+import { decrementQuantity, incrementQuantity, removeFromCart } from "../../redux/slices/cartSlice";
 import axios from "axios";
 import { FaTrashAlt } from "react-icons/fa";
 import "./CartCard.css";
+import Swal from "sweetalert2";
 const { REACT_APP_API_URL } = process.env;
+
+const stockAlert=()=>{
+  Swal.fire({
+    position: "top-center",
+    icon: "error",
+    title: "Out of stock",
+    showConfirmButton: false,
+    timer: 1500,
+  })
+}
 
 export default function CartCard({ name, id, quantity, price }) {
   const dispatch = useDispatch();
@@ -15,7 +22,7 @@ export default function CartCard({ name, id, quantity, price }) {
     const cant = { quantity: quantity };
     const { data } = await axios.put(`${REACT_APP_API_URL}/cart/${id}`, cant);
     if (data === "Out of stock")
-      return window.alert("No more product in stock");
+      return stockAlert();
     dispatch(incrementQuantity(id));
   };
   return (
