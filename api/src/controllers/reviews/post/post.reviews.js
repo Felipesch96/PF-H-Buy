39e7a4cb422385c7488a867a7949d0b1a35a0390
cristopher-99 <cristo_lazo_@hotmail.com>
2023-03-01 21:54:05 -0300

    reviews

--- api/src/controllers/reviews/post/post.reviews.js
+++ api/src/controllers/reviews/post/post.reviews.js
@@ -5,16 +5,19 @@ const postReview = async (req, res) => {
     const {comment, qualification, product_id} = req.body;
     const product= await Products.findById(product_id)
     const newReview = new Review({
-        comment, qualification, product_id
+        comment, 
+        qualification,
+        product_id,
     });
-    
-    try {
+
+  try {
         // const newReview = new Review(data);
         // await newReview.save();
         const savedReview= await newReview.save()
         Products.reviews= Products.reviews.concat(savedReview)
         await Products.save();
         res.status(201).send(savedReview);
+
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
