74495b349fbac8529f94da61f75f3ddf7c3cfe8e
Nicolas <nicolas.iconicoff@gmail.com>
2023-03-02 21:02:03 -0300

    fix delete

--- api/src/routes/products.router.js
+++ api/src/routes/products.router.js
@@ -5,11 +5,13 @@ const { getProducts, productById } = require("../controllers/products/get/get.pr
 const { updateProduct } = require("../controllers/products/put/put.products");
 const { updateVisits } = require("../controllers/products/put/put.products");
 const upload = require("../helpers/multer");
+const { deleteProduct } = require("../controllers/products/delete/delete.products");
 
 router.get("/", getProducts);
 router.get("/:id", productById);
 router.post("/", createNewProduct, upload.single('image'));
 router.put("/visits", updateVisits);
 router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
