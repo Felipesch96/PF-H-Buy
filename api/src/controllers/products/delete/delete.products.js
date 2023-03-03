609b82f250fc9960092d73672d1c67f31a992609
EmijHernandez <100807932+emijhernandez@users.noreply.github.com>
2023-03-03 13:03:53 -0300

    borrado logico

--- api/src/controllers/products/delete/delete.products.js
+++ api/src/controllers/products/delete/delete.products.js
@@ -5,14 +5,18 @@ const productsCtrl = {};
 productsCtrl.deleteProduct = async (req, res) => {
   
   const { id } = req.params;
-  const productById = await Product.findById(id);
+  const ProductById = await Product.findById(id);
 
   try {
-    if (productById.isActive) {
-      res.status(200).send("please deactivate the product before deleting from the database");
-    } else {
-      await Product.findByIdAndDelete(id);
-      res.status(200).send("successfully removed");
+    if (ProductById.isActive==true) {
+      await Product.findOneAndUpdate({ _id: id }, { isActive: false });
+    res.status(200).send("successfully removed");
+    }else if(ProductById.isActive==false){
+      await Product.findOneAndUpdate({ _id: id }, { isActive: true });
+      res.status(200).send("successfully added");
+    }
+     else {
+    res.status(200).send("this product not exist");
     }
   } catch (error) {
     res.status(400).send(error.message);
