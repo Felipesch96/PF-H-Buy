c7e78e5df72a6d97be443826dd4c60ee497f0a68
Felipe Schwindt <felipe.schwindt36@gmail.com>
2023-03-03 13:51:57 -0300

    minor changes

--- api/src/controllers/products/delete/delete.products.js
+++ api/src/controllers/products/delete/delete.products.js
@@ -6,27 +6,15 @@ const productsCtrl = {};
 productsCtrl.deleteProduct = async (req, res) => {
   
   const { id } = req.params;
-  const ProductById = await Product.findById(id);
+  const productById = await Product.findById(id);
 
   try {
-<<<<<<< HEAD
-    if (ProductById.isActive==true) {
-      await Product.findOneAndUpdate({ _id: id }, { isActive: false });
-    res.status(200).send("successfully removed");
-    }else if(ProductById.isActive==false){
-      await Product.findOneAndUpdate({ _id: id }, { isActive: true });
-      res.status(200).send("successfully added");
-    }
-     else {
-    res.status(200).send("this product not exist");
-=======
     if (productById.isActive) {
       res.status(200).send("please deactivate the product before deleting from the database");
     } else {
       await cloudinary.uploader.destroy(productById.img_public_id);
       await Product.findByIdAndDelete(id);
       res.status(200).send("successfully removed");
->>>>>>> 0cc4279b5c35d1bd1678208453292872dc05ca83
     }
   } catch (error) {
     res.status(400).send(error.message);
