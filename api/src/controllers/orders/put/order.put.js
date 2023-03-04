fe13d75b5abc126345d17535330c2e008f29289b
Felipe Schwindt <felipe.schwindt36@gmail.com>
2023-03-04 16:10:12 -0300

    hola

--- api/src/controllers/orders/put/order.put.js
+++ api/src/controllers/orders/put/order.put.js
@@ -1,3 +1,4 @@
+const Product = require ('../../../schemas/Products')
 const Order = require('../../../schemas/Order')
 const orderCtrl = {};
 
@@ -8,9 +9,7 @@ orderCtrl.updateOrder = async (req, res) => {
     const { id } = req.params;
     const data = req.body
     try {
-      console.log('orden', req.body)
-     const ordencita = await Order.findByIdAndUpdate(id, data);
-     console.log(ordencita)
+     await Order.findByIdAndUpdate(id, data);
       res.status(200).send("updated with success");
     } catch (error) {
       res.status(400).send(error.message);
@@ -20,18 +19,14 @@ orderCtrl.updateOrder = async (req, res) => {
   orderCtrl.completedOrder = async(req,res) => {
     const { id } = req.params;
     try {
-      const currentOrder = Order.findById(id)
+      const currentOrder = await Order.findById(id)
       currentOrder.items.map(async (element) => {
         const inCartProduct = await Product.findById(element.product);
         const newStock = inCartProduct.stock > 0 ? inCartProduct.stock - element.quantity : 0  
         await Product.findByIdAndUpdate(element.product, { stock : newStock })
         }) 
-        res.status(201).send({
-          msg: 'order completed',
-          newOrder
-        });
+        res.status(201).send("Stock updated");
     } catch (error) {
-      console.log(error)
       res.status(400).send(error.message);
     }
   }
